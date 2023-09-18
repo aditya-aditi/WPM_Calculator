9cbddeacdd6cb5c08dcda6e8730cbfd5693e0673
aditya-aditi <adityarajsah2008@gmail.com>
2023-09-18 20:30:11 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+import random
+import time
+
+
+time_start = 0
+# Event Handlers
+def start_writing():
+    global time_start
+    txt_write_para.config(state=tk.NORMAL)
+    time_start = time.time()
+
+
+def generate_para():
+    paragraphs = [
+        "The sun dipped below the horizon, painting the sky in hues of orange and pink. As darkness crept in, the stars emerged, sprinkling the night canvas with their shimmering light. A gentle breeze rustled the leaves, and the world seemed to exhale as the day came to a close.",
+        "The old bookstore stood with its weathered facade, inviting passersby with the aroma of old paper and ink. Shelves upon shelves of books lined the walls, each one telling a different story. It was a sanctuary for book lovers, a place where time seemed to slow down as they immersed themselves in the worlds created by words.",
+        "The little girl giggled as she chased after the butterflies, her laughter echoing through the meadow. The butterflies flitted around her, dancing in the sunlight. Her eyes sparkled with wonder as she tried to catch one, but they always slipped through her tiny fingers, leaving her in fits of laughter.",
+        "The rain poured relentlessly, drumming on the roof and windows. Inside, a cozy fireplace crackled, casting a warm glow over the room. The sound of raindrops created a soothing melody, and the scent of wet earth filled the air. It was a perfect day to stay indoors with a good book and a cup of hot cocoa.",
+        "The old oak tree stood tall and proud, its gnarlaed branches reaching toward the sky. It had witnessed generations pass by, and its bark held the stories of time. Birds nested in its branches, and children sought refuge in its shade on sunny days. It was a silent guardian, standing firm through the changing seasons.",
+        "The city came alive as the night descended. Neon lights illuminated the streets, and the hustle and bustle of life continued. Each person carried their own story, their dreams, and aspirations, woven into the tapestry of the urban landscape. In the midst of the chaos, there was a strange beauty in the symphony of lives intertwining.",
+        "The aroma of freshly baked bread filled the bakery, drawing people in like a magnet. The baker worked with practiced hands, kneading the dough with love and skill. Each loaf was a work of art, and as they emerged from the oven, they were met with appreciative smiles from customers eager to savor the warm goodness."
+    ]
+    rand_num = random.randint(0, len(paragraphs)-1)
+    # txt_para.insert("1.0", paragraphs[rand_num])
+    random_para = paragraphs[rand_num]
+    return random_para
+
+def calculate_wpm():
+    txt_para = txt_write_para.get("1.0", tk.END)
+    words = txt_para.split()
+    wpm = len(words) / ((time.time() - time_start)/60)
+    # txt_wpm = tk.Text(height=10, width=50, padx=10, pady=10, font=('helvetica', 14))
+    # txt_wpm.insert("1.0", "WPM: " + str(round(wpm, 2)))
+    # txt_wpm.pack(fill=tk.BOTH)
+    # todo: find a way to display the wpm in the GUI instead of the console
+    print("WPM: " + str(wpm))
+
+# Tkinter Window
+window = tk.Tk()
+window.title("WPM Calculator")
+window.geometry("800x600")
+
+window.rowconfigure([0, 1], weight=1)
+window.columnconfigure([0], weight=1)
+
+frame_para = tk.Frame(relief=tk.SUNKEN, borderwidth=3)
+frame_para.grid(row=0, column=0, sticky="nsew", pady=10, padx=10)
+
+# lbl_para = tk.Label(master=frame_para ,text="The paragraph")
+# lbl_para.pack()
+
+txt_para = tk.Text(master=frame_para, font=('helvetica', 14), width=20)
+txt_para.insert("1.0", generate_para())
+txt_para.config(state=tk.DISABLED)
+txt_para.pack(fill=tk.BOTH)
+
+txt_write_para = tk.Text(height=10, width=50, padx=10, pady=10, font=('helvetica', 14))
+txt_write_para.config(state=tk.DISABLED)
+txt_write_para.grid(row=1, column=0, sticky="nsew", padx=10, pady=10)
+
+frame_btn = tk.Frame()
+frame_btn.grid(row=2, column=0, sticky="nsew", padx=10, pady=10)
+
+btn_calculate_wpm = tk.Button(master=frame_btn, text="Calculate WPM", height=3, width=50, command=calculate_wpm)
+btn_calculate_wpm.pack(side=tk.LEFT, padx=10, pady=10)
+
+btn_start_writing = tk.Button(master=frame_btn, text="Start Writing", height=3, width=50, command=start_writing)
+btn_start_writing.pack(side=tk.LEFT, padx=10, pady=10)
+
+window.mainloop()
